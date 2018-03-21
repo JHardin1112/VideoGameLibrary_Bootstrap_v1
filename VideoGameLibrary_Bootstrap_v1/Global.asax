38b20700d49cf2fa1361458f4193a8570af22c9a
Jeremy Hardin <jeremyhardin@gmail.com>
2018-03-20 20:36:09 -0400

    Add project files.

---
+++ VideoGameLibrary_Bootstrap_v1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="VideoGameLibrary_Bootstrap_v1.MvcApplication" Language="C#" %>
---
+++ VideoGameLibrary_Bootstrap_v1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VideoGameLibrary_Bootstrap_v1.Startup))]
+namespace VideoGameLibrary_Bootstrap_v1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
